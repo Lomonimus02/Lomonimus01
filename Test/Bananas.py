420efea5b198fd550c0a11d587b22de84878bf7f
Lomonimus02 <Lomonimus01@mail.ru>
2025-04-24 09:31:43 +0300

    Add files via upload

--- Test/Bananas.py
+++ Test/Bananas.py
@@ -5,12 +5,12 @@ def minEatingSpeed(piles, h):
 
     while low <= high:
         k = low + (high - low) // 2
-        total_hours = 0
-        for pile_size in piles:
-            hours_for_pile = (pile_size + k - 1) // k
-            total_hours += hours_for_pile
+        t_hours = 0
+        for size in piles:
+            h_for_pile = (size + k - 1) // k
+            t_hours += h_for_pile
 
-        if total_hours <= h:
+        if t_hours <= h:
             result_k = k
             high = k - 1
         else:
@@ -18,4 +18,4 @@ def minEatingSpeed(piles, h):
 
     return result_k
 
-print(minEatingSpeed([12, 3, 42], 12))
+print(minEatingSpeed([7], 3))
